4f11cd08a963a50f8c1d48125e21cf5ff287e021
jacob <104648750+Jakey5197@users.noreply.github.com>
2023-11-04 12:57:18 -0500

    checkbox shenanigans

---
+++ MovieReccApp/DataComms.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieReccApp
+{
+    internal class DataComms
+    {
+        public DataComms()
+        {
+        }
+
+        public getGenres()
+        {
+
+        }
+    }
+}
--- MovieReccApp/MainWindow.xaml.cs
+++ MovieReccApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,7 +29,26 @@ namespace MovieReccApp
 
         private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
+            //Compiling Check Boxes
+            CheckBox[] genres = new CheckBox[8];
+            genres[0] = this.Comedy;
+            genres[1] = this.Romance;
+            genres[2] = this.Horror;
+            genres[3] = this.Action;
+            genres[4] = this.Scifi;
+            genres[5] = this.Mystery;
+            genres[6] = this.Kids;
+            genres[7] = this.Fantasy;
 
+            //Check which boxes are selected
+            foreach (CheckBox genre in genres)
+            {
+                //If checkbox is checked...
+                if(genre.IsChecked == true)
+                {
+
+                }
+            }
         }
     }
 }
