67384e63bab462a522e144a54e26885e73672fa1
jacob <104648750+Jakey5197@users.noreply.github.com>
2023-11-04 11:39:25 -0500

    Basic UI and API

--- MovieReccApp/MainWindow.xaml.cs
+++ MovieReccApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MovieReccApp
         {
             InitializeComponent();
         }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
